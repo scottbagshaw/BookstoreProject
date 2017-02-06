1da656d5daaa1fb00777dc31072213d926d91c4d
unknown <scott.bagshaw@hotmail.co.uk>
2017-02-06 16:34:40 +0000

    Book 3

--- Book.java
+++ Book.java
@@ -1,16 +1,23 @@
 package firstExercise;
 
 public class Book {
+	private static int counter = 0;
+	private int id;
 	private String title;
 	private String author;
 	private String genre;
 	private int pageCount;
 	
 	public Book(String title, String author, String genre, int pageCount) {
+		this.id = counter++;
 		this.title = title;
 		this.author = author;
 		this.genre = genre;
 		this.pageCount = pageCount;
+	}
+	
+	public int getId() {
+		return id;
 	}
 	
 	public String getTitle() {
